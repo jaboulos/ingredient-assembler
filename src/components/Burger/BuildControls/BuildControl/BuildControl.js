4f075b3439c24eae22e8756c34c2fc11a1271ca8
Joseph Boulos <jab12288@gmail.com>
2019-10-20 18:35:33 -0700

    Adding the  build control component

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './buildControl.css';
+
+const buildControl = (props) => (
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>
+      {props.label}
+    </div>
+      <button className={classes.Less}>Remove</button>
+      <button className={classes.More}>Add</button>
+  </div>
+);
+
+export default buildControl;
---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// reusable UI element
+const buildControls = (props) => (
+  <div>
+
+  </div>
+);
+
+export default buildControls;
