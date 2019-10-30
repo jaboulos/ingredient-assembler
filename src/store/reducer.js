87457abcd681c02779be8459d7720934f18c5f08
Joe Boulos <joeboulos@Joes-MacBook-Pro.local>
2019-10-30 12:55:56 -0700

    305: Finishing the reducer for ingredients

---
+++ src/store/reducer.js
@@ -0,0 +1,42 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0,
+    },
+    totalPrice: 4,
+}
+
+// create reducer
+    // manage ingredients state with reducer
+const reducer = ( state = initialState, action ) => {
+    // switch action type
+    switch(action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                // copy state
+                ...state,
+                ingredients: {
+                    // deep clone objects in state
+                    ...state.ingredients,
+                    // overide given ingredient that we get as a payload from action
+                    // ingredientName is a payload from action
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+
+            }
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ...state.ingredientName,
+                [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+            }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
