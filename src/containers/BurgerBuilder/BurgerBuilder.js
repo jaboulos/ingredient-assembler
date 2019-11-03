f81ac15be25accdda90fa05340c5cc87bcc3b1fd
jaboulos <jab12288@gmail.com>
2019-11-03 15:13:57 -0800

    test

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,7 @@ import axios from "../../axios-orders";
 
 import * as burgerBuilderActions from '../../store/actions/index.js'
 
-class BurgerBuilder extends Component {
+class BurgerBuilder extends Component{
 	// constructor(props) {
 	//   super(props);
 	//   this.state {
